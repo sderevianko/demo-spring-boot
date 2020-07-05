47f09b84ca31d55234514b77425fc39f8c2563aa
sderevianko <derevianko.serhii@gmail.com>
2020-07-06 00:53:17 +0300

    Initial commit

---
+++ src/test/java/com/github/sderevianko/demo/InitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sderevianko.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
